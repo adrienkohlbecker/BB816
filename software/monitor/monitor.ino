6d1dbd26fa5561239473233254d5bdeefc47880c
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2022-09-04 17:34:46 +0200

    Add PIN and PORT registers

--- software/monitor/monitor.ino
+++ software/monitor/monitor.ino
@@ -1,26 +1,43 @@
-const char ADDR[] = {27, 26, 25, 24, 23, 22, 21, 20, 45, 44, 43, 42, 41, 40, 39, 38, 35, 34, 33, 32, 31, 30, 29, 28};
-const char DATA[] = {17, 16, 15, 14, 13, 12, 11, 10};
-#define CLOCK_IN 18
+#define DDR_BANK DDRB
+#define PIN_BANK PINB
+#define PORT_BANK PORTB
+#define DDR_ADDR_H DDRF
+#define PIN_ADDR_H PINF
+#define PORT_ADDR_H PORTF
+#define DDR_ADDR_L DDRA
+#define PIN_ADDR_L PINA
+#define PORT_ADDR_L PORTA
+#define DDR_DATA DDRC
+#define PIN_DATA PINC
+#define PORT_DATA PORTC
+#define DDR_CTRL DDRE
+#define PIN_CTRL PINE
+#define PORT_CTRL PORTE
+
+#define CLOCK_SRC 18
 #define CLOCK_EN 6
 #define READ_WRITE 8
 #define SYNC 9
 #define VA 19
+#define MR 5
+#define BE 4
+#define ROM_WE 7
 
 void setup() {
-  for (int n = 0; n < 24; n += 1) {
-    pinMode(ADDR[n], INPUT);
-  }
-  for (int n = 0; n < 8; n += 1) {
-    pinMode(DATA[n], INPUT);
-  }
-  pinMode(CLOCK_IN, INPUT);
+  DDR_BANK = 0b00000000;
+  DDR_ADDR_H = 0b00000000;
+  DDR_ADDR_L = 0b00000000;
+  DDR_DATA = 0b00000000;
+
+  pinMode(CLOCK_SRC, INPUT);
+  pinMode(CLOCK_EN, INPUT);
   pinMode(READ_WRITE, INPUT);
   pinMode(SYNC, INPUT);
   pinMode(VA, INPUT);
+  pinMode(MR, INPUT);
+  pinMode(BE, INPUT);
+  pinMode(ROM_WE, INPUT);
 
-  digitalWrite(CLOCK_EN, 0);
-  pinMode(CLOCK_EN, OUTPUT);;
-  
   Serial.begin(57600);
 }
 
@@ -37,31 +54,34 @@ void startTracing() {
   tracingEnabled = true;
   
   digitalWrite(CLOCK_EN, 1); // enable teensy clock
+  pinMode(CLOCK_EN, OUTPUT);
   __asm__("nop\n\t");        // Wait 62.5ns for main clock to be tri-stated
-  digitalWrite(CLOCK_IN, 0); // make sure it starts with 0
-  pinMode(CLOCK_IN, OUTPUT);
+  digitalWrite(CLOCK_SRC, 0); // make sure it starts with 0
+  pinMode(CLOCK_SRC, OUTPUT);
 }
 
 void stopTracing() {
   tracingEnabled = false;
   
-  pinMode(CLOCK_IN, INPUT);  // tri-state the Teensy clock pin
+  digitalWrite(CLOCK_SRC, 0); 
+  pinMode(CLOCK_SRC, INPUT);  // tri-state the Teensy clock pin
   __asm__("nop\n\t");        // Wait 62.5ns for pin to be tri-stated
   digitalWrite(CLOCK_EN, 0); // enable main clock
+  pinMode(CLOCK_EN, INPUT);
 }
 
 void trace() {
-  digitalWrite(CLOCK_IN, 1);
+  digitalWrite(CLOCK_SRC, 1);
 
   // wait 125ns for the ROM to output its data, each nop has a 62.5ns delay
   __asm__("nop\n\t");
   __asm__("nop\n\t");
 
-  byte bank      = PINB;
-  byte address_h = PINF;
-  byte address_l = PINA;
-  byte data      = PINC;
-  byte ctrl      = PINE;
+  byte bank      = PIN_BANK;
+  byte address_h = PIN_ADDR_H;
+  byte address_l = PIN_ADDR_L;
+  byte data      = PIN_DATA;
+  byte ctrl      = PIN_CTRL;
 
   bool va   = ((ctrl >> 7) & 1) > 0;
   bool sync = ((ctrl >> 1) & 1) > 0;
@@ -83,7 +103,7 @@ void trace() {
   }
   Serial.println(output); 
   
-  digitalWrite(CLOCK_IN, 0);
+  digitalWrite(CLOCK_SRC, 0);
 }
 
 // blockingSerialRead blocks until data is available on the Serial port
